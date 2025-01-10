bfe8285a2932d304d78dea879ed2856cb4b28546
unknown055555 <79971998+unknown055555@users.noreply.github.com>
2025-01-10 12:37:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.querySelector(".first-location .btn2").addEventListener("click", function() {
+    // The URL to redirect to
+    window.location.href = "https://maps.app.goo.gl/k5nxyeJdHXNJYAk68";
+  });
+
+document.querySelector(".left-social-content #fb").addEventListener("click", function() {
+    // The URL to redirect to
+    window.location.href = "https://www.facebook.com/BITSindriDhanbad/";
+  });
+
+document.querySelector(".left-social-content #x").addEventListener("click", function() {
+    // The URL to redirect to
+    window.location.href = "https://x.com/BITSindriDhn";
+  });
+
+document.querySelector(".left-social-content #linkedin").addEventListener("click", function() {
+    // The URL to redirect to
+    window.location.href = "https://www.linkedin.com/school/bit-sindri/";
+  });
+document.querySelector("#homepage").addEventListener("click", function() {
+    // The URL to redirect to
+    window.location.href = "./index.html";
+  });
+document.querySelector("#admission-page").addEventListener("click", function() {
+    // The URL to redirect to
+    window.location.href = "./admission.html";
+  });
+  document.querySelector("#aboutpage").addEventListener("click", function() {
+      // The URL to redirect to
+      window.location.href = "./about.html";
+    });
+document.querySelector(".about-page-redirect").addEventListener("click", function() {
+    // The URL to redirect to
+    window.location.href = "./about.html";
+  });
+
+
